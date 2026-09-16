6a10ec88c959117f61a1fb6b6fff63195e782902
Andrew <ajowe@lanl.gov>
2026-09-16 17:39:05 -0600

    use buhito

--- minervachem/fingerprinters/graphletfingerprinter.py
+++ minervachem/fingerprinters/graphletfingerprinter.py
@@ -1,12 +1,23 @@
-from functools import lru_cache
 import re
-import hashlib
 
 import networkx as nx
-from collections import Counter, defaultdict
+from collections import defaultdict
+from buhito.depth_graphlet import (
+    HashHelper,
+    active_neighbors,
+    generate_subgraphs_depthwise,
+    generate_subgraphs_from_node,
+    search_subgraphs_from_cluster,
+)
 from rdkit.Chem import rdmolops, RWMol
 from rdkit.Chem.rdchem import Mol
 import rdkit
+
+
+# Backwards-compatible name for Minervachem callers. The graphlet enumeration
+# and recursive hashing implementation now lives in Buhito.
+generate_subgraphs = generate_subgraphs_depthwise
+
 
 class GraphletFingerprinter():     
     def __init__(self, max_len=3, useHs=False, elements=(), filter_in=True, terminal_pos=False):
@@ -237,161 +248,9 @@ def mol2_to_nx(mol2str, explicit_h=False):
     G.graph["heavy_reindex"] = heavy_reindex if not explicit_h else None
     return G
 
-# version with nice notation.
-# def active_neighbors(G,node,cur_neighbors,whitelist,yields_with):
-#     # The next things to check will be the neighbors of this neighbor one,
-#     # so long as they aren't already going to be checked!
-#     possible = cur_neighbors | set(G.neighbors(node))
-#     allowed = (possible - yields_with) & whitelist
-#     return allowed
-def active_neighbors(G,node,cur_neighbors,whitelist,yields_with):
-    """
-    Things in current neighbors or neighbors of a given node,
-    as long as in whitelist and not already in current cluster.
-    """
-    """note! this definition is inlined in search_subgraphs from cluster"""
-    return {n for n in (*cur_neighbors,*G.neighbors(node))
-                  if n in whitelist
-                  and n not in yields_with}
-
-def generate_subgraphs_from_node(G,node,depth,whitelist):
-    """
-    Generate subgraphs starting from a specific node.
-    Essentially just sets up calculation for searching from cluster.
-    """
-    blacklist = ()
-    yields_with = {node}
-    neighbors = active_neighbors(G,node,set(),whitelist,yields_with)
-
-    yield from search_subgraphs_from_cluster(G,depth,neighbors,yields_with,whitelist,set())
-
-def search_subgraphs_from_cluster(G,depth,neighbors,yields_with,whitelist,found):
-    "Generate subgraphs from a given cluster."
-    # Depth-first type search
-    f_yw = frozenset(yields_with)
-    if f_yw in found:
-        # Already seen this thing, ignore it
-        return
-    found.add(f_yw)
-
-    yield yields_with
-    if depth<=1: # stop recursion at some arbitrary integer.
-        # Note: if you change this integer, you must change the hasher recursive call to generate_subgraphs!
-        return
-
-    # go deeper!
-
-    for n in neighbors:
-        # next_neighbors = active_neighbors(G, n, neighbors, whitelist, yields_with)
-        # the above gets called enough that inlining actually helps.
-        yields_with.add(n) # add to current set for yielding
-        next_neighbors={n for n in (*neighbors, *G.neighbors(n))
-         if n in whitelist
-         and n not in yields_with}
-        # inception!
-        yield from search_subgraphs_from_cluster(G,depth-1,next_neighbors,yields_with,
-                                                 whitelist,found)
-        yields_with.remove(n) # remove from current set for yielding
-
-    return
-    
-
-def generate_subgraphs(G,maxlen,hash_helper=None,whitelist=None):
-    """
-    G: networkx graph for molecule
-    maxlen: size of
-    hash_helper: used to compute hashes when this function is called recursively
-    whitelist: subset of atoms in the molecule to consider.
-    """
-    # If hash_helper is None, we are generating fresh.
-    # if hash_helper is not None, we are helping ourselves.
-
-    retain_counts=(hash_helper is None)
-    if retain_counts:
-        hash_helper=HashHelper(G,maxlen)
-
-    all_subsets = set()
-    if whitelist is None:
-        whitelist = set(G.nodes)
-    else:
-        whitelist = set(whitelist) # copies if set, removes 'frozen' if frozenset
-
-    for n in list(whitelist): # changes during iteration
-        for subset in generate_subgraphs_from_node(G,n,maxlen,whitelist):
-            subset = frozenset(subset)
-            h = hash_helper(subset)
-            all_subsets.add((subset,h))         
-            
-        whitelist.remove(n) # now never look at that node again
-    if retain_counts:
-        res = Counter((len(ss),h) for ss,h in all_subsets)
-    else:
-        res = None
-    return all_subsets,res
-
 def flatten_list(xss):
     return [x for xs in xss for x in xs]
 
-class HashHelper():
-    def __init__(self, graph, maxlen):
-        self.graph=graph
-        self.maxlen=maxlen
-
-    @staticmethod
-    def custom_hash(obj):
-        s = str(obj).encode()
-        hash_int = int(hashlib.sha1(s).hexdigest(), 16)
-        return hash_int
-
-    @lru_cache(maxsize=None) # very important, this is memoized.
-    def __call__(self,indices):
-
-        if len(indices)==1:
-            # Just hash atom key
-            a=list(indices)[0]
-            akey = self.graph.nodes[a]['atom_key']
-            h = self.custom_hash(akey)
-            return h
-        if len(indices)==2:
-            # Include bond key in hash
-            a1,a2=indices
-            h1, h2 = map(self,((a1,),(a2,)))
-            btype = self.graph.edges[a1,a2]['bond_key']
-            h3 = self.custom_hash(btype)
-            h = self.custom_hash(tuple(sorted((h1, h2, h3))))
-            return h
-        
-        # hash abd count of all substructures of this structure that are at most one smaller than this one.
-        # Note that the his call passes itself back to generate_subgraphs to increase the efficiency
-        # of memoization.
-        sub_graph_set,_ = generate_subgraphs(self.graph,len(indices)-1,hash_helper=self,whitelist=indices)
-        sub_graph_set = [sg for sg, h in sub_graph_set]
-        sub_hashes = Counter(self(idxs) for idxs in sub_graph_set)
-        # combine hashes from substructures to form new hash for this.
-        this_hashkey = tuple(sorted(sub_hashes.items()))
-        h = self.custom_hash(this_hashkey)
-
-        return h
-                                 
-# def ComputeGraphletFingerprint(rdkit_mol, maxlen, explicit_h):
-#     """RDKit style wrapper to generate_subgraphs but keyed by (size, bit)"""
-#     if isinstance(rdkit_mol, Mol):
-#         G = mol_to_nx(rdkit_mol, explicit_h=explicit_h)
-#     elif isinstance(rdkit_mol,str):
-#         if 'TRIPOS' in rdkit_mol:
-#             G = mol2_to_nx(rdkit_mol,explicit_h=explicit_h)
-#         else:
-#             raise ValueError('Unknown molecule type for featurizing')
-#     else:
-#         raise ValueError('Unknown molecule type for featurizing')
-#     subsets, counter = generate_subgraphs(G, maxlen)
-#     bit_info = defaultdict(lambda: [])
-#     for atoms, bit in subsets:
-#         subset = list(atoms)
-#         size = len(subset)
-#         bit_info[(size, bit+2**64)].append(subset)
-#     fp = {(k[0], k[1]+2**64): v for k, v in counter.items()}
-#     return fp, dict(bit_info)
 
 def ComputeGraphletFingerprint(rdkit_mol, maxlen, explicit_h, elements=(), filter_in=True, terminal_pos=False, verbose=False):
     """RDKit style wrapper to generate_subgraphs but keyed by (size, bit)"""
