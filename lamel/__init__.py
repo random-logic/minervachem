133432e4a7fffb823124785ef5441454eb935cad
Andrew <ajowe@lanl.gov>
2026-09-21 15:21:54 -0600

    fix example data path

--- lamel/__init__.py
+++ lamel/__init__.py
@@ -1,6 +1,6 @@
 """LAMEL: an optional meta-learning add-on for MinervaChem."""
 
-from .dataset_loader import LoadedDataset
-from .lamel_learner import MetaLearner
+from lamel.dataset_loader import LoadedDataset
+from lamel.lamel_learner import MetaLearner
 
 __all__ = ["LoadedDataset", "MetaLearner"]
